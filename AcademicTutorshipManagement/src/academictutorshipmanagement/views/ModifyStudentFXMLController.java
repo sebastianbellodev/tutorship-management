b6aace3dc930977294eb74de4be788ff37658597
sebastianbello <sebtrejo@outlook.es>
2023-04-09 20:10:13 -0600

    Implementation of the 'Query Student', 'Modify Student', and 'Assign Student to Academic Tutor' use cases (100%)

---
+++ AcademicTutorshipManagement/src/academictutorshipmanagement/views/ModifyStudentFXMLController.java
@@ -0,0 +1,239 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package academictutorshipmanagement.views;
+
+import academictutorshipmanagement.model.dao.StudentDAO;
+import academictutorshipmanagement.model.pojo.AcademicPersonnel;
+import academictutorshipmanagement.model.pojo.EducationalProgram;
+import academictutorshipmanagement.model.pojo.SchoolPeriod;
+import academictutorshipmanagement.model.pojo.Student;
+import academictutorshipmanagement.utilities.Constants;
+import academictutorshipmanagement.utilities.Utilities;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author sebtr
+ */
+public class ModifyStudentFXMLController implements Initializable {
+
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField paternalSurnameTextField;
+    @FXML
+    private TextField maternalSurnameTextField;
+    @FXML
+    private TextField registrationNumberTextField;
+    @FXML
+    private TextField educationalProgramTextField;
+    @FXML
+    private TextField emailAddressTextField;
+    @FXML
+    private TextField queryTextField;
+
+    private ArrayList<Student> students;
+
+    private AcademicPersonnel academicPersonnel;
+    private SchoolPeriod schoolPeriod;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button deleteButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        nameTextField.editableProperty().bind(registrationNumberTextField.textProperty().isNotEmpty());
+        paternalSurnameTextField.editableProperty().bind(registrationNumberTextField.textProperty().isNotEmpty());
+        maternalSurnameTextField.editableProperty().bind(registrationNumberTextField.textProperty().isNotEmpty());
+    }
+
+    public void configureView(SchoolPeriod schoolPeriod, AcademicPersonnel academicPersonnel) {
+        this.schoolPeriod = schoolPeriod;
+        this.academicPersonnel = academicPersonnel;
+        students = StudentDAO.getStudentsByEducationalProgram(academicPersonnel.getUser().getEducationalProgram().getIdEducationalProgram());
+    }
+
+    @FXML
+    private void cancelButtonClick(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StudentManagementMenuFXML.fxml"));
+        try {
+            Parent root = loader.load();
+            StudentManagementMenuFXMLController studentManagementMenuFXMLController = loader.getController();
+            studentManagementMenuFXMLController.configureView(schoolPeriod, academicPersonnel);
+            Scene mainMenuView = new Scene(root);
+            Stage stage = (Stage) nameTextField.getScene().getWindow();
+            stage.setScene(mainMenuView);
+            stage.setTitle("Gestión de estudiantes.");
+            stage.show();
+        } catch (IOException exception) {
+            System.err.println("The StudentManagementMenuFXML.fxml' file could not be open. Please try again later.");
+        }
+    }
+
+    private boolean validateEmptyField() {
+        return nameTextField.getText().isEmpty()
+                || paternalSurnameTextField.getText().isEmpty()
+                || maternalSurnameTextField.getText().isEmpty();
+    }
+
+    @FXML
+    private void acceptButtonClick(ActionEvent event) {
+        if(!registrationNumberTextField.textProperty().isEmpty().get()) {
+            updateStudent();
+        }
+    }
+
+    private void updateStudent() {
+        if (validateEmptyField()) {
+            Utilities.showAlert("No se puede dejar ningún campo vacío.\n\n"
+                    + "Por favor, compruebe la información ingresada e inténtelo nuevamente.\n",
+                    Alert.AlertType.WARNING);
+        } else {
+            String registrationNumber = registrationNumberTextField.getText();
+            String name = nameTextField.getText();
+            String paternalSurname = paternalSurnameTextField.getText();
+            String maternalSurname = maternalSurnameTextField.getText();
+            String emailAddress = emailAddressTextField.getText();
+            EducationalProgram educationalProgram = academicPersonnel.getUser().getEducationalProgram();
+            Student student = new Student(name, paternalSurname, maternalSurname, emailAddress);
+            student.setRegistrationNumber(registrationNumber);
+            student.setEducationalProgram(educationalProgram);
+            updateStudent(student);
+            queryTextField.clear();
+            students.clear();
+            students = StudentDAO.getStudentsByEducationalProgram(academicPersonnel.getUser().getEducationalProgram().getIdEducationalProgram());
+        }
+    }
+    
+    private void updateStudent(Student student) {
+        int responseCode = StudentDAO.updateStudent(student);
+        switch (responseCode) {
+            case Constants.CORRECT_OPERATION_CODE:
+                Utilities.showAlert("La información se registró correctamente en el sistema.",
+                        Alert.AlertType.WARNING);
+                clearTextField();
+                break;
+            case Constants.INVALID_DATA_ENTERED_CODE:
+                Utilities.showAlert("Los datos ingresados son inválidos.\n\n"
+                        + "Por favor, compruebe la información ingresada e inténtelo nuevamente.\n",
+                        Alert.AlertType.WARNING);
+                break;
+            default:
+                Utilities.showAlert("No hay conexión con la base de datos.\n\n"
+                        + "Por favor, inténtelo más tarde.\n",
+                        Alert.AlertType.ERROR);
+                break;
+        }
+    }
+
+    @FXML
+    private void queryButtonClick(ActionEvent event) {
+        clearTextField();
+        for (Student student : students) {
+            if (student.getRegistrationNumber().equalsIgnoreCase(queryTextField.getText())) {
+                getStudent(student);
+            }
+        }
+    }
+
+    private void getStudent(Student student) {
+        nameTextField.setText(student.getName());
+        registrationNumberTextField.setText(student.getRegistrationNumber());
+        emailAddressTextField.setText(student.getEmailAddress());
+        paternalSurnameTextField.setText(student.getPaternalSurname());
+        maternalSurnameTextField.setText(student.getMaternalSurname());
+        educationalProgramTextField.setText(academicPersonnel.getUser().getEducationalProgram().getName());
+    }
+
+    private void clearTextField() {
+        nameTextField.clear();
+        registrationNumberTextField.clear();
+        emailAddressTextField.clear();
+        paternalSurnameTextField.clear();
+        maternalSurnameTextField.clear();
+        educationalProgramTextField.clear();
+    }
+
+    @FXML
+    private void deleteButtonClick(ActionEvent event) {
+        if(!registrationNumberTextField.textProperty().isEmpty().get()) {
+            deleteStudent();
+        }
+    }
+    
+    private void deleteStudent() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(null);
+        alert.setHeaderText(null);
+        alert.setContentText("¿Desea eliminar la información del sistema?\n\n Esta acción no podrá revertirse.\n");
+        Optional<ButtonType> button = alert.showAndWait();
+        if(button.get() == ButtonType.OK) {
+            deleteAcademicTutorshipReportStudent(registrationNumberTextField.getText());
+            queryTextField.clear();
+            students.clear();
+            students = StudentDAO.getStudentsByEducationalProgram(academicPersonnel.getUser().getEducationalProgram().getIdEducationalProgram());
+        }
+    }
+    
+    private void deleteAcademicTutorshipReportStudent(String registrationNumber) {
+        int responseCode = StudentDAO.deleteAcademicTutorshipReportStudent(registrationNumber);
+        switch (responseCode) {
+            case Constants.CORRECT_OPERATION_CODE:
+                deleteStudent(registrationNumber);
+                clearTextField();
+                break;
+            case Constants.INVALID_DATA_ENTERED_CODE:
+                Utilities.showAlert("Los datos ingresados son inválidos.\n\n"
+                        + "Por favor, compruebe la información ingresada e inténtelo nuevamente.\n",
+                        Alert.AlertType.WARNING);
+                break;
+            default:
+                Utilities.showAlert("No hay conexión con la base de datos.\n\n"
+                        + "Por favor, inténtelo más tarde.\n",
+                        Alert.AlertType.ERROR);
+                break;
+        }
+    }
+    
+    private void deleteStudent(String registrationNumber) {
+        int responseCode = StudentDAO.deleteStudent(registrationNumber);
+        switch (responseCode) {
+            case Constants.CORRECT_OPERATION_CODE:
+                Utilities.showAlert("La información se eliminó correctamente en el sistema.",
+                        Alert.AlertType.WARNING);
+                break;
+            case Constants.INVALID_DATA_ENTERED_CODE:
+                Utilities.showAlert("Los datos ingresados son inválidos.\n\n"
+                        + "Por favor, compruebe la información ingresada e inténtelo nuevamente.\n",
+                        Alert.AlertType.WARNING);
+                break;
+            default:
+                Utilities.showAlert("No hay conexión con la base de datos.\n\n"
+                        + "Por favor, inténtelo más tarde.\n",
+                        Alert.AlertType.ERROR);
+                break;
+        }
+    }
+}
