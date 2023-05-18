f1780e7fcf5ed6694183c415cfff70e138affbda
Pxrsona <obando_armando28@hotmail.com>
2023-05-17 21:51:31 -0600

    updated configureAcademicProblem

--- AcademicTutorshipManagement/src/academictutorshipmanagement/views/ModifyAcademicTutorshipReportFXMLController.java
+++ AcademicTutorshipManagement/src/academictutorshipmanagement/views/ModifyAcademicTutorshipReportFXMLController.java
@@ -271,7 +271,16 @@ public class ModifyAcademicTutorshipReportFXMLController implements Initializabl
 
     @Override
     public void configureAcademicProblem(AcademicProblem academicProblem) {
-        academicProblems.add(academicProblem);
+        if(academicProblem.getIdAcademicProblem() == Constants.PRIMARY_KEY_OF_NON_EXISTENT_RECORD_IN_DATABASE){
+            academicProblems.add(academicProblem);
+        }else{
+            int index = 0;
+            for(AcademicProblem academic: academicProblems){
+                if(academic.getIdAcademicProblem() == academicProblem.getIdAcademicProblem()){
+                    academicProblems.set(index, academicProblem);
+                    return;
+                }
+            }
+        }
     }
-
 }
